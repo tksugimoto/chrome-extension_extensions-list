a27decfb34bb35f5c92f3bfef9dfa98e8f8df8cf
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2015-07-19 13:21:19 +0900

    他の拡張・アプリの変更時にメニューに反映

--- background.js
+++ background.js
@@ -7,52 +7,39 @@ chrome.browserAction.onClicked.addListener(function (){
 	});
 });
 
+
 var KEY_ID_CONTEXT_MENU_ALL = "a";
 var KEY_ID_CONTEXT_MENU_NOT_HAS_OPTION_PAGE = "b";
 
-chrome.contextMenus.create({
-	id: KEY_ID_CONTEXT_MENU_NOT_HAS_OPTION_PAGE,
-	title: "一覧（オプション無し）",
-	contexts: ["browser_action"]
-});
-chrome.contextMenus.create({
-	id: KEY_ID_CONTEXT_MENU_ALL,
-	title: "一覧（ALL）",
-	contexts: ["browser_action"]
-});
 
-[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].forEach(function (value){
+createMenu();
+chrome.management.onInstalled.addListener(createMenu);
+chrome.management.onUninstalled.addListener(createMenu);
+chrome.management.onEnabled.addListener(createMenu);
+chrome.management.onDisabled.addListener(createMenu);
+
+function createMenu (){
+	chrome.contextMenus.removeAll();
 	chrome.contextMenus.create({
-		title: "test" + value,
-		contexts: ["browser_action"],
-		onclick: function (info, tab){
-			
-		}
+		id: KEY_ID_CONTEXT_MENU_NOT_HAS_OPTION_PAGE,
+		title: "一覧（オプション無し）",
+		contexts: ["browser_action"]
 	});
-});
-
-
-chrome.management.getAll(function (list){
-	list.sort(function (a, b){
-		if (a.shortName > b.shortName) return 1;
-		if (a.shortName < b.shortName) return -1;
-		return 0;
+	chrome.contextMenus.create({
+		id: KEY_ID_CONTEXT_MENU_ALL,
+		title: "一覧（ALL）",
+		contexts: ["browser_action"]
 	});
-	list.forEach(function (extensionInfo /*, index, all */){
-		if (extensionInfo.enabled && extensionInfo.installType === "development") {
-			chrome.contextMenus.create({
-				parentId: KEY_ID_CONTEXT_MENU_ALL,
-				title: extensionInfo.shortName,
-				contexts: ["browser_action"],
-				onclick: function (info, tab){
-					chrome.tabs.create({
-						url: "chrome://extensions/?id=" + extensionInfo.id
-					});
-				}
-			});
-			if (!extensionInfo.optionsUrl) {
+	chrome.management.getAll(function (list){
+		list.sort(function (a, b){
+			if (a.shortName > b.shortName) return 1;
+			if (a.shortName < b.shortName) return -1;
+			return 0;
+		});
+		list.forEach(function (extensionInfo /*, index, all */){
+			if (extensionInfo.enabled && extensionInfo.installType === "development") {
 				chrome.contextMenus.create({
-					parentId: KEY_ID_CONTEXT_MENU_NOT_HAS_OPTION_PAGE,
+					parentId: KEY_ID_CONTEXT_MENU_ALL,
 					title: extensionInfo.shortName,
 					contexts: ["browser_action"],
 					onclick: function (info, tab){
@@ -61,7 +48,19 @@ chrome.management.getAll(function (list){
 						});
 					}
 				});
+				if (!extensionInfo.optionsUrl) {
+					chrome.contextMenus.create({
+						parentId: KEY_ID_CONTEXT_MENU_NOT_HAS_OPTION_PAGE,
+						title: extensionInfo.shortName,
+						contexts: ["browser_action"],
+						onclick: function (info, tab){
+							chrome.tabs.create({
+								url: "chrome://extensions/?id=" + extensionInfo.id
+							});
+						}
+					});
+				}
 			}
-		}
+		});
 	});
-});
+}
