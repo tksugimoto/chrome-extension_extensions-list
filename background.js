985f2dbfc05e0eeb25731386cda27005d9583ae4
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2017-01-07 21:26:41 +0900

    [Clean] 関数定義を(効果的な範囲で) function から アロー演算子に変更

--- background.js
+++ background.js
@@ -9,19 +9,19 @@ const KEY_ID_CONTEXT_MENU_OPEN_EXT_PAGE = "c";
 chrome.runtime.onInstalled.addListener(readyToCreate);
 chrome.runtime.onStartup.addListener(readyToCreate);
 
-chrome.management.onInstalled.addListener(function (extensionInfo){
+chrome.management.onInstalled.addListener(extensionInfo => {
 	console.log("onInstalled: " + extensionInfo.name);
 	readyToCreate();
 });
-chrome.management.onUninstalled.addListener(function (extensionInfo){
+chrome.management.onUninstalled.addListener(extensionInfo => {
 	console.log("onUninstalled: " + extensionInfo.name);
 	readyToCreate();
 });
-chrome.management.onEnabled.addListener(function (extensionInfo){
+chrome.management.onEnabled.addListener(extensionInfo => {
 	console.log("onEnabled: " + extensionInfo.name);
 	readyToCreate();
 });
-chrome.management.onDisabled.addListener(function (extensionInfo){
+chrome.management.onDisabled.addListener(extensionInfo => {
 	console.log("onDisabled: " + extensionInfo.name);
 	readyToCreate();
 });
@@ -34,15 +34,15 @@ function readyToCreate(){
 	timeoutId = window.setTimeout(createMenu, 100);
 }
 function createMenu (){
-	const count = (function () {
+	const count = (() => {
 		let i = 0;
-		return function () {
+		return () => {
 			return i++;
 		}
 	})();
-	chrome.contextMenus.removeAll(function (){
-		chrome.management.getAll(function (list){
-			list.sort(function (a, b){
+	chrome.contextMenus.removeAll(() => {
+		chrome.management.getAll(list => {
+			list.sort((a, b) => {
 				if (a.shortName > b.shortName) return 1;
 				if (a.shortName < b.shortName) return -1;
 				return 0;
@@ -58,8 +58,8 @@ function createMenu (){
 				id: KEY_ID_CONTEXT_MENU_NOT_HAS_OPTION_PAGE,
 				title: "一覧（オプション無し）",
 				contexts: ["browser_action"]
-			}, function (){
-				list.forEach(function (extensionInfo /*, index, all */){
+			}, () => {
+				list.forEach(extensionInfo => {
 					if (extensionInfo.enabled && extensionInfo.installType === "development") {
 						if (!extensionInfo.optionsUrl) {
 							chrome.contextMenus.create({
@@ -76,8 +76,8 @@ function createMenu (){
 				id: KEY_ID_CONTEXT_MENU_ALL,
 				title: "一覧（ALL）",
 				contexts: ["browser_action"]
-			}, function (){
-				list.forEach(function (extensionInfo /*, index, all */){
+			}, () => {
+				list.forEach(extensionInfo => {
 					if (extensionInfo.enabled && extensionInfo.installType === "development") {
 						chrome.contextMenus.create({
 							parentId: KEY_ID_CONTEXT_MENU_ALL,
@@ -92,7 +92,7 @@ function createMenu (){
 	});
 }
 
-chrome.contextMenus.onClicked.addListener(function (info) {
+chrome.contextMenus.onClicked.addListener(info => {
 	if (info.menuItemId === KEY_ID_CONTEXT_MENU_OPEN_EXT_PAGE) {
 		chrome.tabs.create({
 			url: "chrome://extensions/"
