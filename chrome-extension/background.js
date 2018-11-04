086d4cfdafd41ef64a484eaf41ab200aa28bc266
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2018-11-04 13:35:34 +0900

    style: 文字列の ダブルクォーテーション -> シングルクォーテーション

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,21 +1,21 @@
 
 chrome.browserAction.setBadgeText({
-	text: "ext",
+	text: 'ext',
 });
 
 
-const KEY_ID_CONTEXT_MENU_OPEN_EXT_PAGE = "open_ext_page";
+const KEY_ID_CONTEXT_MENU_OPEN_EXT_PAGE = 'open_ext_page';
 
 chrome.contextMenus.create({
-	title: "拡張ページを表示",
-	contexts: ["browser_action"],
+	title: '拡張ページを表示',
+	contexts: ['browser_action'],
 	id: KEY_ID_CONTEXT_MENU_OPEN_EXT_PAGE,
 });
 
 chrome.contextMenus.onClicked.addListener(info => {
 	if (info.menuItemId === KEY_ID_CONTEXT_MENU_OPEN_EXT_PAGE) {
 		chrome.tabs.create({
-			url: "chrome://extensions/",
+			url: 'chrome://extensions/',
 		});
 	}
 });
--- chrome-extension/popup_script.js
+++ chrome-extension/popup_script.js
@@ -1,19 +1,19 @@
-"use strict";
+'use strict';
 
 {
-	const key = "enable_open_if_candidate_only_one";
-	window.enableOpenIfCandidateOnlyOne = localStorage[key] !== "false";
+	const key = 'enable_open_if_candidate_only_one';
+	window.enableOpenIfCandidateOnlyOne = localStorage[key] !== 'false';
 
-	const checkBox = document.getElementById("enable_open_if_candidate_only_one");
+	const checkBox = document.getElementById('enable_open_if_candidate_only_one');
 	checkBox.checked = enableOpenIfCandidateOnlyOne;
-	checkBox.addEventListener("change", evt => {
+	checkBox.addEventListener('change', evt => {
 		enableOpenIfCandidateOnlyOne = evt.checked;
 		localStorage[key] = enableOpenIfCandidateOnlyOne;
 	});
 }
-const container = document.getElementById("container");
+const container = document.getElementById('container');
 
-const searchExtensionInput = document.getElementById("search-extension");
+const searchExtensionInput = document.getElementById('search-extension');
 searchExtensionInput.focus();
 
 chrome.management.getAll(extensions => {
@@ -27,34 +27,34 @@ chrome.management.getAll(extensions => {
 	});
 
 	extensions.forEach(extension => {
-		const li = document.createElement("li");
-		const elem = document.createElement("a");
+		const li = document.createElement('li');
+		const elem = document.createElement('a');
 		elem.innerText = extension.name;
 		elem.title = extension.description;
-		elem.href = "#";
+		elem.href = '#';
 		const openExtensionPage = () => {
 			chrome.tabs.create({
-				url: "chrome://extensions/?id=" + extension.id,
+				url: 'chrome://extensions/?id=' + extension.id,
 			});
 		};
 		elem.onclick = openExtensionPage;
-		elem.addEventListener("keydown", evt => {
+		elem.addEventListener('keydown', evt => {
 			const key = evt.key.toLowerCase();
-			if (key === " ") {
+			if (key === ' ') {
 				// Spaceキーでも開く
 				openExtensionPage();
-			} else if (key === "o") {
+			} else if (key === 'o') {
 				// oキーでオプションページを開く
 				if (!extension.enabled) return;
 				if (!extension.optionsUrl) return;
 				chrome.tabs.create({
 					url: extension.optionsUrl,
 				});
-			} else if (key === "d") {
+			} else if (key === 'd') {
 				// dキーで拡張を無効化
 				if (!extension.mayDisable) return;
 				chrome.management.setEnabled(extension.id, false);
-			} else if (key === "r") {
+			} else if (key === 'r') {
 				// rキーで拡張をリロード
 				if (!extension.mayDisable) return;
 				if (extension.id === chrome.runtime.id) {
@@ -68,9 +68,9 @@ chrome.management.getAll(extensions => {
 		});
 		const notifyReload = () => {
 			const duration = 2;
-			const notification = document.createElement("span");
-			notification.innerText = "更新完了！";
-			notification.classList.add("reloaded-notification");
+			const notification = document.createElement('span');
+			notification.innerText = '更新完了！';
+			notification.classList.add('reloaded-notification');
 			notification.classList.add(`fadeout-${duration}s`);
 			li.appendChild(notification);
 			setTimeout(() => {
@@ -78,13 +78,13 @@ chrome.management.getAll(extensions => {
 			}, duration * 1000);
 		};
 		if (extension.optionsUrl) {
-			li.classList.add("has-options_page");
+			li.classList.add('has-options_page');
 		}
 		if (!extension.enabled) {
-			li.classList.add("disabled");
+			li.classList.add('disabled');
 		}
 		if (!extension.mayDisable) {
-			li.classList.add("controlled");
+			li.classList.add('controlled');
 		}
 		li.appendChild(elem);
 		container.appendChild(li);
@@ -95,14 +95,14 @@ chrome.management.getAll(extensions => {
 		const extension = extensions.find(({id}) => id === enabledExtension.id);
 		if (extension) {
 			extension.enabled = true;
-			extension._elem.classList.remove("disabled");
+			extension._elem.classList.remove('disabled');
 		}
 	});
 	chrome.management.onDisabled.addListener(disabledExtension => {
 		const extension = extensions.find(({id}) => id === disabledExtension.id);
 		if (extension) {
 			extension.enabled = false;
-			extension._elem.classList.add("disabled");
+			extension._elem.classList.add('disabled');
 		}
 	});
 
@@ -117,12 +117,12 @@ chrome.management.getAll(extensions => {
 			return words.every(w => text.includes(w));
 		};
 		extensions.forEach(extension => {
-			const target = extension.name + "\n" + extension.description;
+			const target = extension.name + '\n' + extension.description;
 			if (isMatch(target.toLowerCase())) {
-				extension._elem.style.display = "";
+				extension._elem.style.display = '';
 				matchedExtensionIds.push(extension.id);
 			} else {
-				extension._elem.style.display = "none";
+				extension._elem.style.display = 'none';
 			}
 		});
 		if (!enableOpenIfCandidateOnlyOne) {
@@ -130,12 +130,12 @@ chrome.management.getAll(extensions => {
 		}
 		if (openIfCandidateOnlyOne && matchedExtensionIds.length === 1) {
 			chrome.tabs.create({
-				url: "chrome://extensions/?id=" + matchedExtensionIds[0],
+				url: 'chrome://extensions/?id=' + matchedExtensionIds[0],
 			});
 		}
 	}
 
-	searchExtensionInput.addEventListener("compositionupdate", () => {
+	searchExtensionInput.addEventListener('compositionupdate', () => {
 		// 日本語変換中
 		window.setTimeout(() => {
 			showCandidate({
@@ -143,7 +143,7 @@ chrome.management.getAll(extensions => {
 			});
 		}, 1);
 	});
-	searchExtensionInput.addEventListener("compositionend", () => {
+	searchExtensionInput.addEventListener('compositionend', () => {
 		// 日本語変換完了・変換キャンセル
 		window.setTimeout(() => {
 			showCandidate({
@@ -151,7 +151,7 @@ chrome.management.getAll(extensions => {
 			});
 		}, 1);
 	});
-	searchExtensionInput.addEventListener("keydown", evt => {
+	searchExtensionInput.addEventListener('keydown', evt => {
 		// Tabキー
 		if (evt.keyCode === 9) return;
 		// 日本語入力中
